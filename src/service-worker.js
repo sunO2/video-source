f7a6fc0adca7cf779d9a171bebc1b42ecd35b9dd
ex-hezhihu870 <ex-hezhihu870@pingan.com.cn>
2025-07-17 17:09:48 +0800

    v1.0.0

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,12 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('v1.0').then((cache) => {
       return cache.addAll([
         'index.html',
         'css/style.css',
-        'js/bg.js'
+        'css/detail.css',
+        'js/bg.js',
+        'js/detail.js'
       ]);
     })
   );
