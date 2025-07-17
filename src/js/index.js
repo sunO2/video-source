38c1a982858c572c612063b54db09b7fd94e0104
ex-hezhihu870 <ex-hezhihu870@pingan.com.cn>
2025-07-17 15:38:38 +0800

    v1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -57,7 +57,20 @@ document.addEventListener('DOMContentLoaded', () => {
             if (componentHandler) {
                 componentHandler.upgradeElement(movieCard);
             }
-        });
+          
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('Service Worker registration failed: ', registrationError);
+      });
+  });
+}
     }
 
     // Custom Tab switching functionality
