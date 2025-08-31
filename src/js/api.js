9e83f3f0c56fb80a484765e9300068a0b8836e71
sunO2 <354137379@qq.com>
2025-08-31 20:06:15 +0800

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
 // API functions for fetching movie data
-const API_BASE_URL = 'https://5bt0.com/prod/api/v1';
+const API_BASE_URL = 'https://web.mukaku.com/prod/api/v1/';
 const APP_ID = '83768d9ad4';
 const IDENTITY = '23734adac0301bccdcb107c4aa21f96c';
 
